c8f733f179537f0681c623daa55e6d8ec77523c8
Marcel Marin <marcelmarin256@gmail.com>
2022-12-23 12:19:31 -0400

    Nav y seccion1 del home iniciados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Section1Component } from './pages/home/section1/section1.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { HomeSection2Component } from './pages/home/home-section2/home-section2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    Section1Component,
+    CursosComponent,
+    HomeSection2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+
 })
 export class AppModule { }
